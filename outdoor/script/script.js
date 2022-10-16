05704b1bebe7859015a64ccc036de45654ddf5ab
SergiiIgn <110237022+SergiiIgn@users.noreply.github.com>
2022-10-16 14:19:03 +0300

    Add files via upload

--- outdoor/script/script.js
+++ outdoor/script/script.js
@@ -1,7 +1,3 @@
-$('.main').parallax({imageSrc: '../img/main/main-bg.png', speed: 0.5});
-
-$('.surfing').parallax({imageSrc: '../img/surfing/sea.png', speed: 0.6});
-
 $('#surfing-link').on('click', function(e){
     e.preventDefault();
     document.getElementById('toggleCheckbox').checked = false
